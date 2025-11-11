d681b1f28a425412e54dc59ee0fd06e784a4dfeb
MichaelAgustinn <michaelagustinn080@gmail.com>
2025-11-11 13:33:41 +0800

    [fix: broken format mail]

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,9 +3,8 @@
     <td class="header" style="background-color: #263C8F; text-align: center; padding: 30px 0;">
         <a href="{{ $url }}" style="display: inline-block; text-decoration: none; text-align: center;">
             {{-- Logo --}}
-            <img src="{{ $message->embed(public_path('images/logo-isc-email.png')) }}" alt="{{ config('app.name') }} Logo"
-                width="80" height="80"
-                style="display:block; border-radius:50%; margin:0 auto 10px; object-fit:cover;">
+            {{-- <img src="{{ public_path('images/logo-isc-email.png') }}" alt="{{ config('app.name') }} Logo" width="80"
+                height="80" style="display:block; border-radius:50%; margin:0 auto 10px; object-fit:cover;"> --}}
 
 
             {{-- Nama Aplikasi --}}
@@ -14,8 +13,8 @@
             </div>
 
             {{-- Garis Aksen Emas --}}
-            <div style="margin: 12px auto 0; width: 80px; height: 4px; background-color: #F7C70F; border-radius: 2px;">
-            </div>
+            {{-- <div style="margin: 12px auto 0; width: 80px; height: 4px; background-color: #F7C70F; border-radius: 2px;">
+            </div> --}}
         </a>
     </td>
 </tr>
