99ccf13441c25778e3379e3357bd131af2838b7a
MichaelAgustinn <michaelagustinn080@gmail.com>
2026-05-02 16:33:23 +0800

    [fix: grid]

--- resources/views/landing/head.blade.php
+++ resources/views/landing/head.blade.php
@@ -704,7 +704,7 @@
 
         .blog-grid {
             display: grid;
-            grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+            grid-template-columns: repeat(3, 1fr);
             gap: 2rem;
         }
 
