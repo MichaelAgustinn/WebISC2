12c6a197f9a4676c97e033da6c26931e7a9c845a
MichaelAgustinn <michaelagustinn080@gmail.com>
2026-05-09 02:51:57 +0800

    [fix: routing detail karya]

--- resources/views/admin/projects/monitoring.blade.php
+++ resources/views/admin/projects/monitoring.blade.php
@@ -76,13 +76,13 @@
                                 </p>
 
 
-                                <a href="{{ $project->slug }}" target="_blank"
+                                <a href="/creation/{{ $project->slug }}" target="_blank"
                                     class="inline-flex items-center gap-1 text-xs font-medium text-blue-600 hover:text-blue-800 hover:underline transition">
                                     <svg class="w-3 h-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                                     </svg>
-                                    Lihat Detial
+                                    Lihat Detail
                                 </a>
 
                             </td>
