7a83b56b032e287e111faf974c053b30b5bde795
MichaelAgustinn <michaelagustinn080@gmail.com>
2026-05-13 03:19:45 +0800

    [fix: query]

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -13,18 +13,36 @@ class AdminUserController extends Controller
 {
     public function index(Request $request)
     {
+        $currentUser = Auth::user();
         $query = User::query();
 
-        if ($request->has('search')) {
-            $query->where('name', 'like', '%' . $request->search . '%')
-                ->orWhere('email', 'like', '%' . $request->search . '%');
+        if ($currentUser->role != 'admin' && $currentUser->profile) {
+            $divisionTarget = $currentUser->profile->division;
+            $validDivisions = ['mobile', 'website', 'uiux', 'sistem_cerdas', 'iot'];
+
+            if (in_array($divisionTarget, $validDivisions)) {
+                $query->with('profile')
+                    ->where('id', '!=', $currentUser->id)
+                    ->whereHas('profile', function ($q) use ($divisionTarget) {
+                        $q->where('division', $divisionTarget);
+                    });
+            }
         }
 
-        $users = $query->latest()->paginate(10);
+        if ($request->has('search') && $request->search != '') {
+            $searchTerm = $request->search;
+            $query->where(function ($q) use ($searchTerm) {
+                $q->where('name', 'like', '%' . $searchTerm . '%')
+                    ->orWhere('email', 'like', '%' . $searchTerm . '%');
+            });
+        }
+
+        $users = $query->paginate(10);
+
+        $users->appends($request->all());
 
         return view('admin.users.index', compact('users'));
     }
-
     public function unverified(Request $request)
     {
         $query = Regist::query();
